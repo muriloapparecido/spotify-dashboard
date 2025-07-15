e15282126857d59014933d8331339bba686629bb
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-07-14 23:24:47 -0500

    Fetch top tracks from the past four weeks

---
+++ fetchTopTracks.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = process.env.SPOTIFY_TOKEN
+
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+async function getTopTracks(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  return (await fetchWebApi(
+    'v1/me/top/tracks?time_range=short_term&limit=5', 'GET'
+  )).items;
+}
+
+async function main() {
+    const topTracks = await getTopTracks();
+    console.log(
+      topTracks?.map(
+        ({ name, artists }) =>
+          `${name} by ${artists.map(artist => artist.name).join(', ')}`
+      )
+    );
+  }
+  
+  main();
+  
