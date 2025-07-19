7850c7726195cca3a00133883cc895a42e8c58a7
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-07-18 22:59:35 -0500

    Fetch User Profile and Display user profile picture

---
+++ public/js/fetchUserProfile.js
@@ -0,0 +1,58 @@
+import  { refreshAccessToken } from './callback.js'; 
+
+//Ensure access token is valid
+async function ensureValidToken() {
+  const token = sessionStorage.getItem('access_token'); 
+  const expiresAt = sessionStorage.getItem('expires_at');
+
+  if (!token || !expiresAt || Date.now() > parseInt(expiresAt)) {
+    console.log('Access token expired or missing. Refreshing...');
+    await refreshAccessToken();
+  }
+}
+
+//Make Web API call
+async function fetchWebApi(endpoint, method, body) {
+  const accessToken = sessionStorage.getItem('access_token'); 
+  const headers = {
+      Authorization: `Bearer ${accessToken}`,
+  }; 
+  if (method !== 'GET'){
+    headers['Content-Type'] = 'applications/json'; 
+  }
+
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    method, 
+    headers, 
+    body: method !== 'GET' && body ? JSON.stringify(body) : null, 
+    }); 
+
+  return await res.json();
+}
+
+//Get user profile
+async function getUserProfile(){
+  // Endpoint reference : https://developer.spotify.com/documentation/web-api/reference/get-users-top-artists-and-tracks
+  const response = await fetchWebApi( 'v1/me', 'GET' );
+
+  return response;
+}
+
+async function main() {
+    await ensureValidToken(); 
+
+    const userProfile = await getUserProfile(); 
+    const list = document.getElementById('user-profile')
+
+    const img = document.createElement('img'); 
+    img.src = userProfile.images?.[0]?.url || ''; 
+    img.alt = `${userProfile.display_name}'s Profile Picture`;
+    img.style.width = '100px';
+    img.style.height = '100px';
+    img.style.borderRadius = '50%'; 
+
+    list.appendChild(img); 
+}
+  
+main();
+  
