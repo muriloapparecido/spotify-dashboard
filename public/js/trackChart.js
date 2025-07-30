24b8b035ad446fa45228119b9437a2e2afa6d5cd
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-07-30 00:37:40 -0500

    Remove js chart logic for loader debugging through devtools

--- public/js/trackChart.js
+++ public/js/trackChart.js
@@ -83,7 +83,7 @@ export function renderComparisonChart( shortTermTracks, midTermTracks, longTermT
         }); 
 
         //Hide loader, show chart
-        document.getElementById("chartLoader").style.display = "none"; 
-        document.getElementById("chartContainer").style.display = "flex"
+        //document.getElementById("chartLoader").style.display = "none"; 
+        //document.getElementById("chartContainer").style.display = "flex"
     }, 300); 
 }
